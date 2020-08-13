07d3bc6fcb82b516e8f9ecfd73d40969773a04da
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2020-08-13 12:01:39 +0100

    test

--- pre_commit_hooks/php-strict-types.sh
+++ pre_commit_hooks/php-strict-types.sh
@@ -3,11 +3,12 @@ missing=()
 for x in "$@"; do
   if ! grep -q '^declare *(strict_types *= *1);$' $x; then
     missing+=($x)
+    sed -i 's/<?php/<?php declare_strict_types(true);/g' $x
   fi
 done
 
 if (( $#missing > 0 )); then
-  echo "The following files do not declare strict_types:" 1>&2
+  echo "The following files had strict mode added:" 1>&2
   print -lu 2 $missing
 fi
 exit $#missing
